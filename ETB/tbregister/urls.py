c77286a7c5ed4322ccc20f9770b085d73c20206e
Abdul Basit <basitirfan760@gmail.com>
2022-09-21 21:05:31 +0500

    Common Lab testtype page done

--- ETB/tbregister/urls.py
+++ ETB/tbregister/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from.  import views
+from django.urls import path,include
+from . import  views
 
 urlpatterns = [
-    path('', views.homepage, name="index")
+    path('',views.index,name='Home'),
+    path('login',views.login,name='Login'),
+    path('enroll' , views.enroll, name='Enroll'),
+    path('enroll2',views.enroll_two,name='Enroll2'),
+    path('enroll3',views.actual_enroll,name='Enroll3' ),
+    path('commonlab' , views.commonlab,name='commonlab')
 ]
--- ETB/tbregister/views.py
+++ ETB/tbregister/views.py
@@ -1,5 +1,34 @@
+from urllib import response
 from django.shortcuts import render
+import requests
 
 
-def homepage(request):
-    return render(request, 'tbregister/base.html')
+# Create your views here.
+
+BASE_URL = 'http://46.20.206.173:18080/openmrs/ws/rest/v1/'
+
+def index(req):
+    return render(req,'tbregister/base.html')
+
+def login(req):
+    return render(req,'tbregister/login.html')
+
+def enroll(req):
+    return render(req,'tbregister/enroll_with_form.html')
+
+    
+def enroll_two(req):
+    return render(req,'tbregister/enroll_without_form.html')
+
+def actual_enroll(req):
+    return render(req,'tbregister/actual_enroll_form.html')
+
+
+def commonlab(req):
+    url = 'commonlab/labtesttype'
+    response = requests.get(url=BASE_URL+url,params={'v' : 'default'})
+    
+    context = {
+        'response' : response.json()['results'] 
+    }
+    return render(req,'tbregister/commonlab.html',context=context)
