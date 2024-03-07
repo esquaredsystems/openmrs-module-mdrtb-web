ee08678771b25e7726199891003d10c21aecb5f0
Esquared Systems <owais.a.hussain@gmail.com>
2024-03-07 09:22:23 +0500

    Several refactoring steps

--- mdrtb/utilities/patient_utils.py
+++ mdrtb/utilities/patient_utils.py
@@ -351,7 +351,7 @@ def get_programs(req, uuid=None, params=None):
     programs = []
     if status:
         for program in response["results"]:
-            if program["retired"] == False:
+            if not program["retired"]:
                 programs.append(program)
         return programs
     else:
