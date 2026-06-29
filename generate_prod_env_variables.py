8d9c18bda38a189f341e1f073fbbd1cce0e2dbc5
seekme_94 <owais.a.hussain@gmail.com>
2026-06-29 20:31:42 +0500

    Update generate_prod_env_variables.py

--- generate_prod_env_variables.py
+++ generate_prod_env_variables.py
@@ -3,11 +3,21 @@ from django.core.management.utils import get_random_secret_key
 import utilities.common_utils as utils
 
 
-def generate_production_envrionment_variables():
-    with open(f"{utils.get_project_root()}/.env", "w", encoding="utf-8") as env_file:
-        env_file.write(f"SECRET_KEY = {get_random_secret_key()}")
+def generate_production_environment_variables():
+    root = utils.get_project_root()
+    example_path = root / ".env-example"
+    env_path = root / ".env"
+
+    with open(example_path, "r", encoding="utf-8") as f:
+        template = f.read()
+
+    env_content = template.replace("your-secret-key-here", get_random_secret_key())
+
+    with open(env_path, "w", encoding="utf-8") as f:
+        f.write(env_content)
+
     if not os.path.exists("logs"):
         os.makedirs("logs")
 
 
-generate_production_envrionment_variables()
+generate_production_environment_variables()
